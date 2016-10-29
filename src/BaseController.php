8490ecd7331bed384fe02d8de05414afb3321fda
root <root@ip-172-31-26-202.ap-south-1.compute.internal>
2016-10-29 06:43:58 +0530

    indentation and a minor fix

--- src/BaseController.php
+++ src/BaseController.php
@@ -8,35 +8,36 @@ class BaseController {
 	protected $POST_Map;
 
 	public function run() {
-            $action = isset($_REQUEST['action'])?$_REQUEST['action']:null;
-            $this->setMaps();
-            $method = $_SERVER['REQUEST_METHOD'];
-            if ($method == 'GET') {
-                $this->runGet($action);
-            } else if ($method == 'POST') {
-                $this->runPost($action);
-            }
+		$action = isset($_REQUEST['action'])?$_REQUEST['action']:null;
+		$this->setMaps();
+		$method = $_SERVER['REQUEST_METHOD'];
+		if ($method == 'GET') {
+			$this->runGet($action);
+		} else if ($method == 'POST') {
+			$this->runPost($action);
+		}
 	}
 
-    protected function runGet($action) {
-        $this->runMethod($action,$this->GET_Map);
-    }
+	protected function runGet($action) {
+		$this->runMethod($action,$this->GET_Map);
+	}
 
-    protected function runPost($action) {
-        $this->runMethod($action,$this->POST_Map);
-    }
+	protected function runPost($action) {
+		$this->runMethod($action,$this->POST_Map);
+	}
 
-    protected function runMethod($action,$map) {
-        if ($action && $map && isset($map[$action])) {
-            $func = $map[$action];
-            $func();
-        } else {
-            $this->defaultAction();
-        }
-    }
+	protected function runMethod($action,$map) {
+		if ($action && $map && isset($map[$action])) {
+			$func = $map[$action];
+			$func();
+		} else {
+			$this->defaultAction();
+		}
+	}
 
 	protected function setMaps() {
-		$this->map = array();
+		$this->GET_map = array();
+		$this->POST_map = array();
 	}
 
 	protected function defaultAction() {
