a86e64544fa107bc6c3c14610bc7ff7c78922617
John Doe <johndoe@example.com>
2023-10-25 14:52:52 +0530

    mysql 8 related changes

--- tests/test_data/user_creds.sql
+++ tests/test_data/user_creds.sql
@@ -1,10 +1,8 @@
+CREATE USER 'r_user'@'%' IDENTIFIED BY 'r_pwd';
 GRANT SELECT ON testdb_mvc.* TO 'r_user'@'%';
-ALTER USER 'r_user'@'%' IDENTIFIED BY 'r_pwd';
 
+CREATE USER 'w_user'@'%' IDENTIFIED BY 'w_pwd';
 GRANT SELECT,INSERT,UPDATE,DELETE ON testdb_mvc.* TO 'w_user'@'%';
-ALTER USER 'w_user'@'%' IDENTIFIED BY 'w_pwd';
 
+CREATE USER 'a_user'@'%' IDENTIFIED BY 'a_pwd';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,REFERENCES,ALTER ON testdb_mvc.* TO 'a_user'@'%';
-ALTER USER 'a_user'@'%' IDENTIFIED BY 'a_pwd';
-
-FLUSH PRIVILEGES;
